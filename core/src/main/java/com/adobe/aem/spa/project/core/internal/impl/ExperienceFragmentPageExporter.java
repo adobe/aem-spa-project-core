ba73b728305fc0901720a619a3b871a51e76d85d
Niek Raaijmakers <raaijmak@adobe.com>
2021-06-01 06:23:21 +0200

    Remove hierarchynodeexporter - this is not required

--- core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
+++ core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
@@ -8,6 +8,6 @@ import com.adobe.cq.export.json.hierarchy.HierarchyNodeExporter;
  * Interface that allows experience fragment pages (that cannot inherit the core component page) to be exported as model JSON
  * so that the SPA editor can function properly.
  */
-public interface ExperienceFragmentPageExporter extends HierarchyNodeExporter, ContainerExporter {
+public interface ExperienceFragmentPageExporter extends ContainerExporter {
 
 }
