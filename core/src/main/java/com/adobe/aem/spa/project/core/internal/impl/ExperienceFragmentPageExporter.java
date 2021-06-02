fcbbc96ab950f870be69332188908ac4e0030508
Niek Raaijmakers <raaijmak@adobe.com>
2021-06-02 18:05:11 +0200

    Remove unused import

--- core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
+++ core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
@@ -2,7 +2,6 @@ package com.adobe.aem.spa.project.core.internal.impl;
 
 
 import com.adobe.cq.export.json.ContainerExporter;
-import com.adobe.cq.export.json.hierarchy.HierarchyNodeExporter;
 
 /**
  * Interface that allows experience fragment pages (that cannot inherit the core component page) to be exported as model JSON
