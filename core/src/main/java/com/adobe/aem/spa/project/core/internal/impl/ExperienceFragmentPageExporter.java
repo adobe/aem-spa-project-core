038de4db7c9645d05f37e2fdf8d90369301ee559
Niek Raaijmakers <raaijmak@adobe.com>
2021-06-25 07:59:22 +0200

    Adding HierarchyNodeExporter to fix XF's not being editable

--- core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
+++ core/src/main/java/com/adobe/aem/spa/project/core/internal/impl/ExperienceFragmentPageExporter.java
@@ -2,11 +2,12 @@ package com.adobe.aem.spa.project.core.internal.impl;
 
 
 import com.adobe.cq.export.json.ContainerExporter;
+import com.adobe.cq.export.json.hierarchy.HierarchyNodeExporter;
 
 /**
  * Interface that allows experience fragment pages (that cannot inherit the core component page) to be exported as model JSON
  * so that the SPA editor can function properly.
  */
-public interface ExperienceFragmentPageExporter extends ContainerExporter {
+public interface ExperienceFragmentPageExporter extends ContainerExporter, HierarchyNodeExporter {
 
 }
